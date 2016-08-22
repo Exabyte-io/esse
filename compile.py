edf4acaf1cc8011c718b45ca732d6844032e5839
Michael Haverty <michael@exabyte.io>
2016-08-22 16:07:25 -0700

    Enabled passing argument to compile.py to set the element used for the templates

--- compile.py
+++ compile.py
@@ -15,6 +15,7 @@ group = parser.add_argument_group('Required arguments')
 parser.add_argument('-m', '--minify', default=False, action="store_true", help="Minify json.")
 parser.add_argument('-f', '--file', default=False, action="store", dest='file_mode', help="Single json file")
 parser.add_argument('-d', '--dir', default=True, action="store", dest='dir_mode', help="Top of directory tree containing json files")
+parser.add_argument('-e', '--element', default="Si", action="store", dest='element', help="Element to be used in the calculation")
 
 args = parser.parse_args()
 
@@ -40,6 +41,10 @@ def compile_json_dir(dir):
         for file in files:
             if file.endswith(".json"):
                 if file.endswith("_template.json"):
+                    with open(file, 'r') as f2:
+                        newText = f2.read().replace('USER_SPECIFIED_ELEMENT', args.element)
+                    with open(file, 'w') as f3:
+                        f3.write(newText)
                     compile_json_file_template(os.path.join(root.replace("compiled\/examples","example"),file))
                     file = file.replace("_template","")
                 print root, file
