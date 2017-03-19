45265f19c65648ed41bf283eba6fe75fd13f00b3
Alexander Smirnoff <alex@exabyte.io>
2017-03-19 10:04:52 -0700

    SOF-2031: creating a package to reuse schemas in workflow editor and other projects

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var {jsonInclude} = require('./unit-schema-include');
+var {templateList} = require('./unit-schema-include');
+
+module.exports = {
+    jsonInclude: jsonInclude,
+    templateList: templateList
+};
