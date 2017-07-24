cdb4b8fe6e9415583c5489c4fc97f173027d0f36
Mohammad Mohammadi <mohammad@exabyte.io>
2017-07-24 12:36:04 -0700

    Updated dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='exabyte_materials_json',
+    name='exabyte-materials-json',
     version='0.1.0',
     description='Contains schemas and examples for materials and simulations related data in JSON representation.',
     url='https://github.com/Exabyte-io/exabyte-materials-json',
     author='Exabyte Inc.',
     author_email='info@exabyte.io',
-    packages=find_packages(exclude=['examples', 'docs', 'tests*']),
+    packages=find_packages(exclude=['example', 'lib', 'tests']),
     install_requires=[
+        "pyyaml==3.12",
+        "jsonschema==2.6.0",
+        "json_include==0.1.4"
     ],
     classifiers=[
         'Programming Language :: Python',
