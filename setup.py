46a3a05e34d6b74352c9b12b4ffdcd3e164fd844
Mohammad Mohammadi <mohammad@exabyte.io>
2017-08-04 11:13:14 -0700

    Removed egg

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "json_include==0.2.9"
     ],
     dependency_links=[
-        "git+https://git@github.com/Exabyte-io/json_include.git@452de29147df52a38fd6130590373555228486ba#egg=json_include-0.2.9"
+        "git+https://git@github.com/Exabyte-io/json_include.git@452de29147df52a38fd6130590373555228486ba"
     ],
     classifiers=[
         'Programming Language :: Python',
