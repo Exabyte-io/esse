e842b0bdbbff5d42d39c712af63c99271f919436
Mohammad Mohammadi <mohammad@exabyte.io>
2019-01-08 16:50:47 -0800

    Refactor tests

--- tests/validate.js
+++ tests/validate.js
@@ -1,30 +1,26 @@
+import file from "file";
+import path from "path";
 import chai from "chai";
-import prettyjson from "prettyjson";
-import {ajvHandler as ajv, examples, getSchemaByIdBasename} from "../src/index";
 
-function getSchemaAndExpectOK(basename) {
-    const schema = getSchemaByIdBasename(basename);
-    chai.expect(ajv.getSchema(schema.id)).to.be.ok;
-}
+import {ESSE} from "../src/index";
+import {EXAMPLES_DIR, SCHEMAS_DIR} from "../src/settings";
+import {JSONSchemaResolver} from "../src/resolver/resolver";
 
-describe('test handpicked schemas', function () {
-    it('primitive schema', function () {
-        getSchemaAndExpectOK("1d_data_series.json")
-    });
-    it('schema with external ref', function () {
-        getSchemaAndExpectOK("atomic_coordinates.json")
-    });
-});
+const esse = new ESSE();
+const jsonResolver = new JSONSchemaResolver();
+
 
 describe('validate all examples', function () {
-    examples.forEach(example => {
-        it(`${example.id} should be valid`, function () {
-            const validator = ajv.getSchema(example.id);
-            const valid = validator(example.content);
-            if (!valid) {
-                console.log(prettyjson.render(validator.errors));
-            }
-            chai.expect(valid).to.be.ok;
+    file.walkSync(EXAMPLES_DIR, (dirPath_, dirs_, files_) => {
+        files_.forEach(file_ => {
+            const examplePath = path.join(dirPath_, file_);
+            const schemaPath = examplePath.replace(EXAMPLES_DIR, SCHEMAS_DIR);
+            it(`${examplePath.replace(`${EXAMPLES_DIR}/`, "")}`, function () {
+                const example = jsonResolver.resolve(examplePath);
+                const schema = jsonResolver.resolve(schemaPath);
+                const valid = esse.validate(example, schema);
+                chai.expect(valid).to.be.ok;
+            })
         });
     });
 });
--- tests/validate.py
+++ tests/validate.py
@@ -1,24 +1,38 @@
 import os
+import unittest
 
 from esse import ESSE, SCHEMA_DR, EXAMPLE_DR
+
+
+class TestCase(unittest.TestCase):
+
+    def __init__(self, example_path):
+        super(TestCase, self).__init__(methodName="runTest")
+        self.example_path = example_path
+        self.schema_path = self.example_path.replace(EXAMPLE_DR, SCHEMA_DR)
+        self.esse = ESSE()
+
+    def runTest(self):
+        schema = self.esse._get_json(self.schema_path)
+        example = self.esse._get_json(self.example_path)
+        self.esse.validate(example, schema)
+
+
+class TestResult(unittest.TextTestResult):
+    def getDescription(self, test):
+        return test.example_path.replace("".join((EXAMPLE_DR, "/")), "")
 
 
 def validate_examples():
     """
     Validates examples against their schemas.
     """
-    es = ESSE()
+    suite = unittest.TestSuite()
     for root, dirs, files in os.walk(EXAMPLE_DR):
         for file_ in files:
-            example_path = os.path.join(root, file_)
-            schema_path = example_path.replace(EXAMPLE_DR, SCHEMA_DR)
-            try:
-                schema = es._get_json(schema_path)
-                example = es._get_json(example_path)
-                es.validate(example, schema)
-            except:
-                print("{} is invalid!".format(example_path))
-                raise
+            TestCase(example_path=os.path.join(root, file_)).run()
+            suite.addTest(TestCase(example_path=os.path.join(root, file_)))
+    unittest.TextTestRunner(verbosity=2, resultclass=TestResult).run(suite)
 
 
 if __name__ == "__main__":
