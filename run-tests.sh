db6f04c03785ccc76447283f9f1ee7514d8babcd
Mohammad Mohammadi <mohammad@exabyte.io>
2019-01-08 18:22:11 -0800

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -11,8 +11,19 @@ pip -q install -r ${DIR}/requirements.txt
 export PYTHONPATH=${DIR}:${PYTHONPATH}
 
 # javascript tests
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
 npm install
 npm test
 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 # python tests
 python tests/validate.py
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
