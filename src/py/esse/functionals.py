74392da11b3f714319acfdb5c30cd0389e90b980
Alexander Zech <alexander.zech@unige.ch>
2020-08-06 13:15:27 -0700

    Apply minor corrections (id strings, docstrings, etc)

--- src/py/esse/functionals.py
+++ src/py/esse/functionals.py
@@ -4,11 +4,11 @@ import os
 from esse.utils import read_json_file, dump_json_file
 
 DIR = os.path.dirname(__file__)
-UNIT_PATH = os.path.join(DIR, "data/schema/models_directory/pb/qm/dft")
-PROTO_FILE = os.path.join(UNIT_PATH, "dft_unit_functionals_proto.json")
-UNIT_FILE = os.path.join(UNIT_PATH, "dft_unit_functionals.json")
+UNIT_FUNCTIONALS_PATH = os.path.join(DIR, "data/schema/models_directory/pb/qm/dft")
+PROTOTYPE_FILENAME = os.path.join(UNIT_FUNCTIONALS_PATH, "dft_unit_functionals_proto.json")
+UNIT_FILENAME = os.path.join(UNIT_FUNCTIONALS_PATH, "dft_unit_functionals.json")
 
-PROP_TEMPLATE = Template("""{
+SCHEMA_WITH_PROPERTIES_TEMPLATE = Template("""{
 "properties": {
     "name": {
         "enum": [
@@ -29,14 +29,20 @@ PROP_TEMPLATE = Template("""{
 }""")
 
 def generate_dft_unit_functionals():
-    proto = read_json_file(PROTO_FILE)
+    """
+    Generate list of functionals suitable for validation by 'oneOf'.
+
+    The targeted JSON file has a convoluted form which makes it inconvenient
+    to read and prone to errors. Thus, it is generated automatically from
+    a prototype file that is easier to maintain.
+    """
+    proto = read_json_file(PROTOTYPE_FILENAME)
     del proto["description"]
     output = {}
     for rung, configs in proto.iteritems():
         o = {"oneOf": []}
         for config in configs:
-            tmp_string = PROP_TEMPLATE.substitute(config)
-            o["oneOf"].append(json.loads(tmp_string))
+            o["oneOf"].append(json.loads(SCHEMA_WITH_PROPERTIES_TEMPLATE.substitute(config)))
         output[rung] = o
 
-    dump_json_file(UNIT_FILE, output)
+    dump_json_file(UNIT_FILENAME, output)
