a83d50f111e9be88ef475692064b3456a1d2f666
Jonathan Hanson <jonathan@exabyte.io>
2020-12-11 17:29:52 -0800

    Decided on a full release bump, instead of an incremental bugfix release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ EXTRA_FILES.extend(get_files_by_path(os.path.join(DIR, "manifest")))
 
 setup(
     name='esse',
-    version='2020.10.19.4',
+    version='2020.12.14',
     description='Exabyte Source of Schemas and Examples',
     long_description=long_description,
     long_description_content_type='text/markdown',
