f77b72e83064217bfa73094854f87044f2c1f18b
adewyer <adewyer@umich.edu>
2022-02-10 10:58:53 -0800

    removed uneccessary lines

--- functionals.py
+++ functionals.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import json
-import errno
 from string import Template
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -9,7 +8,6 @@ REL_DIR = "schema/models_directory/pb/qm/dft"
 UNIT_FUNCTIONALS_PATH = os.path.join(BASE_DIR, REL_DIR)
 PROTOTYPE_FILENAME = os.path.join(UNIT_FUNCTIONALS_PATH, "dft_unit_functionals_proto.json")
 UNIT_FILENAME = os.path.join(UNIT_FUNCTIONALS_PATH, "dft_unit_functionals.json")
-DATA_FILENAME = os.path.join(BASE_DIR, "src/py/esse/data", REL_DIR, "dft_unit_functionals.json")
 
 SCHEMA_WITH_PROPERTIES_TEMPLATE = Template("""{
 "properties": {
@@ -63,7 +61,6 @@ def generate_dft_unit_functionals():
         output[rung] = o
 
     remove(UNIT_FILENAME)
-    remove(DATA_FILENAME)
 
     with open(UNIT_FILENAME, "w") as f:
         f.write("".join((json.dumps(output, separators=(',', ': '), indent=4, sort_keys=True), "\n")))
