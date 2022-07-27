6d9314f74b28fa4e91205c0602b30da94e1cdd67
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2022-07-27 03:04:09 +0300

    update: generate schemas directly into index.js

--- build_schemas.js
+++ build_schemas.js
@@ -3,7 +3,12 @@
  * at build time and writes them out to a single schemas.js file for
  * downstream consumption to avoid FS calls in the browser.
  */
- const fs = require("fs");
- const {ESSE} = require("./lib/js/esse");
- const {schemas} = new ESSE();
- fs.writeFileSync("./lib/js/esse/schemas.js", "module.exports = {schemas: " + JSON.stringify(schemas) + "}", "utf8");
+const fs = require("fs");
+const { ESSE } = require("./lib/js/esse");
+
+const { schemas } = new ESSE();
+fs.appendFileSync(
+    "./lib/js/esse/index.js",
+    "\n\nvar _schemas = "+JSON.stringify(schemas)+"; \n\nexports.schemas = _schemas;",
+    "utf8",
+);
