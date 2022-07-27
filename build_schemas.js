77d1dcb697dbe9350dfff9132008d14cc8c89441
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2022-07-27 16:17:28 +0300

    chore: add comments to build_schemas script

--- build_schemas.js
+++ build_schemas.js
@@ -1,14 +1,20 @@
 /**
  * build_schemas uses node API to read all schemas from the FS
- * at build time and writes them out to a single schemas.js file for
+ * at build time and appends them to the main index.js file for
  * downstream consumption to avoid FS calls in the browser.
  */
 const fs = require("fs");
 const { ESSE } = require("./lib/js/esse");
 
 const { schemas } = new ESSE();
+
+/**
+ * Add additional export of all ESSE schemas at the end of the index.js
+ * file to be able to import them in the same way as the ESSE class:
+ * const { ESSE, schemas } from '@exabyte-io/esse.js';
+ */
 fs.appendFileSync(
     "./lib/js/esse/index.js",
-    "\n\nvar _schemas = "+JSON.stringify(schemas)+"; \n\nexports.schemas = _schemas;",
+    "\n\nexports.schemas = "+JSON.stringify(schemas)+";",
     "utf8",
 );
