46d95b0da62821df808646d4b1111948939d9987
Alexander Zech <alexzech@gmx.de>
2022-09-20 18:07:34 -0700

    chore: build RJSF schema from tree
    Example using DFT tree

--- scripts/build_rjsf_schemas.py
+++ scripts/build_rjsf_schemas.py
@@ -4,10 +4,11 @@ from typing import Tuple, Union, List
 
 
 class LabeledNode:
-    def __init__(self, value, label):
+    def __init__(self, value: str, label: str) -> None:
         self.label = label
         self.value = value
         self.nodes = []
+        self.default = False
 
     def add_node(self, lab_node: "LabeledNode") -> "LabeledNode":
         if isinstance(lab_node, LabeledNode) and self.has_correct_label(lab_node):
@@ -32,38 +33,32 @@ class LabeledNode:
         return [node.value for node in self.nodes]
 
     @property
-    def child_label(self):
+    def child_label(self) -> Union[str, None]:
         if not self.has_children:
             return None
         return self.nodes[0].label
 
+    def set_default(self, is_default) -> "LabeledNode":
+        self.default = is_default
+        return self
 
-def build_dependency(parent: str, child: str, option_map: dict) -> dict:
-    cases = [build_case(parent, child, option={p: c}) for p, c in option_map.items()]
-    return {
-        "dependencies": {
-            parent: {
-                "oneOf": cases
-            }
-        }
-    }
+    @property
+    def default_option(self) -> str:
+        default_node = LabeledNode.get_default_node(self.nodes)
+        if default_node:
+            return default_node.value
 
+    @staticmethod
+    def get_default_node(nodelist: List["LabeledNode"]) -> Union["LabeledNode", None]:
+        filtered = list(filter(lambda n: n.default, nodelist))
+        if len(filtered) == 1:
+            return filtered[0]
 
-def build_case(parent: str, child: str, option: dict, extra_fields: dict = None) -> dict:
-    if extra_fields is None:
-        extra_fields = {}
-    return {
-        "properties": {
-            parent: {
-                "enum": list(option.keys()),
-                **(extra_fields.get(parent, {}))
-            },
-            child: {
-                "enum": list(chain.from_iterable(option.values())),
-                **(extra_fields.get(child, {}))
-            }
-        }
-    }
+    @staticmethod
+    def get_default_option(nodelist: List["LabeledNode"]) -> str:
+        default_node = LabeledNode.get_default_node(nodelist)
+        if default_node:
+            return default_node.value
 
 
 def recursive_dependency(nodelist: List[LabeledNode]) -> dict:
@@ -74,7 +69,7 @@ def recursive_dependency(nodelist: List[LabeledNode]) -> dict:
         "dependencies": {
             parent_label: {
                 # only parent_values, child_values, and dependencies will change here
-                "oneOf": [build_case_v2(
+                "oneOf": [build_case(
                     parent_name=node.label,
                     parent_value=node.value,
                     child_name=node.child_label,
@@ -85,7 +80,7 @@ def recursive_dependency(nodelist: List[LabeledNode]) -> dict:
     }
 
 
-def build_case_v2(parent_name: str,
+def build_case(parent_name: str,
                   parent_value: str,
                   child_name: str,
                   child_values: list,
@@ -112,27 +107,57 @@ def build_case_v2(parent_name: str,
     }
 
 
-def non_recursive() -> None:
-    o_map = {
-        "1": ["ABC", "BCD"],
-        "2": ["CDE", "DEF"],
+def build_properties(tree: LabeledNode) -> dict:
+    if tree.value != "root":
+        return {}
+    level1_label = tree.child_label
+    level1 = tree.nodes
+    level2_label = tree.nodes[0].child_label
+    level2 = list(chain.from_iterable([c.nodes for c in level1]))
+    level3_label = tree.nodes[0].nodes[0].child_label
+    level3 = list(chain.from_iterable([c.nodes for c in level2]))
+    string_type = {"type": "string"}
+    return {
+        "type": "object",
+        "properties": {
+            level1_label: {
+                **string_type,
+                "enum": tree.options,
+                "default": LabeledNode.get_default_option(level1)
+            },
+            level2_label: {
+                **string_type,
+                "default": LabeledNode.get_default_option(level2)
+            },
+            level3_label: {
+                **string_type,
+                "default": LabeledNode.get_default_option(level3)
+            },
+        }
     }
-    deps = build_dependency("a", "b", o_map)
-    print(deps)
+
+
+def build_rjsf_schema(tree: LabeledNode) -> dict:
+    if tree.value != "root" or not tree.has_children:
+        return {}
+    return {
+        **(build_properties(tree)),
+        **(recursive_dependency(tree.nodes))
+    }
 
 
 def recursive() -> None:
-    root = LabeledNode("root", None)
-    dft = root.add_node(LabeledNode("DFT", "type"))
+    root = LabeledNode("root", "root")
+    dft = root.add_node(LabeledNode("DFT", "type").set_default(True))
     lda = dft.add_node(LabeledNode("LDA", "subtype"))
-    gga = dft.add_node(LabeledNode("GGA", "subtype"))
+    gga = dft.add_node(LabeledNode("GGA", "subtype").set_default(True))
     svwn = lda.add_node(LabeledNode("SVWN", "functional"))
     pz = lda.add_node(LabeledNode("PZ", "functional"))
-    pbe = gga.add_node(LabeledNode("PBE", "functional"))
+    pbe = gga.add_node(LabeledNode("PBE", "functional").set_default(True))
     pw91 = gga.add_node(LabeledNode("PW91", "functional"))
 
-    deps = recursive_dependency(root.nodes)
-    print(json.dumps(deps, indent=2))
+    schema = build_rjsf_schema(tree=root)
+    print(json.dumps(schema, indent=2))
 
 
 if __name__ == "__main__":
