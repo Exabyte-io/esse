53a503d5e436133eccd5e00b7cec18f5c59072c0
Alexander Zech <alexzech@gmx.de>
2022-10-03 12:49:23 -0700

    refactor: use anytree implementation of nodes

--- scripts/build_rjsf_schemas.py
+++ scripts/build_rjsf_schemas.py
@@ -5,68 +5,60 @@ from pathlib import Path
 from anytree import Node, RenderTree
 
 
-class LabeledNode:
-    def __init__(self, value: str, label: str) -> None:
+class ModelNode(Node):
+
+    def __init__(self, name, default: bool = False, label: str = None, value: str = None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.default = default
         self.label = label
         self.value = value
-        self.nodes = []
-        self.default = False
 
-    def __repr__(self):
-        return f"LabeledNode(label={self.label}, value={self.value}, default={self.default})"
+    def items(self) -> Tuple[str, Union[str, List[str]]]:
+        for node in self.children:
+            yield node.label, node.value
 
-    def add_node(self, lab_node: "LabeledNode") -> "LabeledNode":
-        if isinstance(lab_node, LabeledNode) and self.has_correct_label(lab_node):
-            self.nodes.append(lab_node)
-            return lab_node
-
-    def has_correct_label(self, lab_node: "LabeledNode") -> bool:
-        if len(self.nodes) == 0:
+    def has_correct_label(self, lab_node: "ModelNode") -> bool:
+        if not self.has_children:
             return True
-        return all([lab_node.label == node.label for node in self.nodes])
+        return all([lab_node.label == node.label for node in self.children])
+
+    def add_node(self, new_node: "ModelNode") -> "ModelNode":
+        if self.has_correct_label(new_node):
+            self.children = [*self.children, new_node]
+            return new_node
 
     @property
     def has_children(self) -> bool:
-        return len(self.nodes) > 0
-
-    def items(self) -> Tuple[str, Union[str, List[str]]]:
-        for node in self.nodes:
-            yield node.label, node.value
-
-    @property
-    def options(self) -> List[str]:
-        return [node.value for node in self.nodes]
+        return len(self.children) > 0
 
     @property
     def child_label(self) -> Union[str, None]:
         if not self.has_children:
             return None
-        return self.nodes[0].label
+        return self.children[0].label
 
-    def set_default(self, is_default) -> "LabeledNode":
+    @property
+    def options(self) -> List[str]:
+        return [node.value for node in self.children]
+
+    def set_default(self, is_default) -> "ModelNode":
         self.default = is_default
         return self
 
-    @property
-    def default_option(self) -> str:
-        default_node = LabeledNode.get_default_node(self.nodes)
-        if default_node:
-            return default_node.value
-
     @staticmethod
-    def get_default_node(nodelist: List["LabeledNode"]) -> Union["LabeledNode", None]:
+    def get_default_node(nodelist: List["ModelNode"]) -> Union["ModelNode", None]:
         filtered = list(filter(lambda n: n.default, nodelist))
         if len(filtered) == 1:
             return filtered[0]
 
     @staticmethod
-    def get_default_option(nodelist: List["LabeledNode"]) -> str:
-        default_node = LabeledNode.get_default_node(nodelist)
+    def get_default_option(nodelist: List["ModelNode"]) -> str:
+        default_node = ModelNode.get_default_node(nodelist)
         if default_node:
             return default_node.value
 
 
-def recursive_dependency(nodelist: List[LabeledNode]) -> dict:
+def recursive_dependency(nodelist: List[ModelNode]) -> dict:
     if len(nodelist) == 0 or not any([node.has_children for node in nodelist]):
         return {}
     parent_label = nodelist[0].label
@@ -79,7 +71,7 @@ def recursive_dependency(nodelist: List[LabeledNode]) -> dict:
                     parent_value=node.value,
                     child_name=node.child_label,
                     child_values=node.options,
-                    dependencies=recursive_dependency(node.nodes)) for node in nodelist]
+                    dependencies=recursive_dependency(node.children)) for node in nodelist]
             }
         }
     }
@@ -112,15 +104,15 @@ def build_case(parent_name: str,
     }
 
 
-def build_properties(tree: LabeledNode) -> dict:
-    if tree.value != "root":
+def build_properties(tree: ModelNode) -> dict:
+    if not tree.is_root:
         return {}
     level1_label = tree.child_label
-    level1 = tree.nodes
-    level2_label = tree.nodes[0].child_label
-    level2 = list(chain.from_iterable([c.nodes for c in level1]))
-    level3_label = tree.nodes[0].nodes[0].child_label
-    level3 = list(chain.from_iterable([c.nodes for c in level2]))
+    level1_value = tree.children
+    level2_label = tree.children[0].child_label
+    level2_value = list(chain.from_iterable([c.children for c in level1_value]))
+    level3_label = tree.children[0].children[0].child_label
+    level3_value = list(chain.from_iterable([c.children for c in level2_value]))
     string_type = {"type": "string"}
     return {
         "type": "object",
@@ -128,47 +120,49 @@ def build_properties(tree: LabeledNode) -> dict:
             level1_label: {
                 **string_type,
                 "enum": tree.options,
-                "default": LabeledNode.get_default_option(level1)
+                "default": ModelNode.get_default_option(level1_value)
             },
             level2_label: {
                 **string_type,
-                "default": LabeledNode.get_default_option(level2)
+                "default": ModelNode.get_default_option(level2_value)
             },
             level3_label: {
                 **string_type,
-                "default": LabeledNode.get_default_option(level3)
+                "default": ModelNode.get_default_option(level3_value)
             },
         }
     }
 
 
-def build_rjsf_schema(tree: LabeledNode) -> dict:
-    if tree.value != "root" or not tree.has_children:
+def build_rjsf_schema(tree: ModelNode) -> dict:
+    if not tree.is_root or not tree.has_children:
         return {}
     return {
         **(build_properties(tree)),
-        **(recursive_dependency(tree.nodes))
+        **(recursive_dependency(tree.children))
     }
 
 
 def recursive() -> None:
-    root = LabeledNode("root", "root")
-    dft = root.add_node(LabeledNode("density functional theory", "type").set_default(True))
-    lda = dft.add_node(LabeledNode("local density approximation", "subtype"))
-    gga = dft.add_node(LabeledNode("generalized gradient approximation", "subtype").set_default(True))
-    other = dft.add_node(LabeledNode("other", "subtype"))
+    root = ModelNode("root", label="root")
+    dft = ModelNode("dft", parent=root, label="type", value="density functional theory").set_default(True)
+    lda = ModelNode("lda", parent=dft, label="subtype", value="local density approximation")
+    gga = ModelNode("gga", parent=dft, label="subtype", value="generalized gradient approximation").set_default(True)
+    other = ModelNode("other", parent=dft, label="subtype", value="other")
 
-    lda.add_node(LabeledNode("pz", "functional"))
-    lda.add_node(LabeledNode("pw", "functional"))
-    lda.add_node(LabeledNode("vwn", "functional"))
-    lda.add_node(LabeledNode("other", "functional"))
+    ModelNode("pz", parent=lda, label="functional", value="pz")
+    ModelNode("pw", parent=lda, label="functional", value="pw")
+    ModelNode("vwn", parent=lda, label="functional", value="vwn")
+    ModelNode("other", parent=lda, label="functional", value="other")
 
-    gga.add_node(LabeledNode("pbe", "functional").set_default(True))
-    gga.add_node(LabeledNode("pbesol", "functional"))
-    gga.add_node(LabeledNode("pw91", "functional"))
-    gga.add_node(LabeledNode("other", "functional"))
+    ModelNode("pbe", parent=gga, label="functional", value="pbe").set_default(True)
+    ModelNode("pbesol", parent=gga, label="functional", value="pbesol")
+    ModelNode("pw91", parent=gga, label="functional", value="pw91")
+    ModelNode("other", parent=gga, label="functional", value="other")
 
-    other.add_node(LabeledNode("other", "functional"))
+    ModelNode("other", parent=other, label="functional", value="other")
+
+    print(RenderTree(root))
 
     schema = build_rjsf_schema(tree=root)
     print(json.dumps(schema, indent=2))
