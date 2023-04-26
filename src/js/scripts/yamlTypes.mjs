bbb0b3f6ff66d181e0fa59c4fb23618a4695253c
Alexander Zech <alexzech@gmx.de>
2023-04-26 16:29:50 -0700

    chore: use synchronous functions from fs

--- src/js/scripts/yamlTypes.mjs
+++ src/js/scripts/yamlTypes.mjs
@@ -1,40 +1,39 @@
 /* eslint-disable no-restricted-syntax */
-/* eslint-disable no-await-in-loop */
-import fs from "fs/promises";
+import fs from "fs";
 import yaml from "js-yaml";
 import path from "path";
 import lodash from "lodash";
 
 const SCHEMA_DIR = "../../../schema/";
 
-async function walkDir(dir, callback) {
-    const subDirs = await fs.readdir(dir);
+function walkDir(dir, callback) {
+    const subDirs = fs.readdirSync(dir);
 
     for (const subDir of subDirs) {
         const itemPath = path.join(dir, subDir);
-        const stat = await fs.stat(itemPath);
+        const stat = fs.statSync(itemPath);
 
         if (stat.isDirectory()) {
-            await walkDir(itemPath, callback);
+            walkDir(itemPath, callback);
         } else {
-            await callback(itemPath);
+            callback(itemPath);
         }
     }
 }
 
-await walkDir(SCHEMA_DIR, async (filePath) => {
-    if (path.extname(filePath) !== ".yml" || path.extname(filePath) !== ".yaml") {
+walkDir(SCHEMA_DIR, (filePath) => {
+    if (path.extname(filePath) !== ".yml") {
         return;
     }
-
+    console.log(filePath);
     const outFilename = `${path.basename(filePath, ".yml")}.json`;
     const dirname = path.dirname(filePath);
 
-    const fileContents = await fs.readFile(filePath);
+    const fileContents = fs.readFileSync(filePath);
     const obj = yaml.load(fileContents);
     const enumObj = lodash.mapValues(obj, (value) => ({ enum: value }));
 
-    await fs.writeFile(
+    fs.writeFileSync(
         `${path.join(dirname, outFilename)}`,
         `${JSON.stringify(enumObj, null, 4)}\n`,
     );
