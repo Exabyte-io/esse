11f2ab7bbc5b6d34f5df8259ce77365ab6a8fb24
Timur Bazhirov <timur@exabyte.io>
2023-12-24 04:49:37 -0800

    chore: update build schemas script to build docs assets

--- build_schemas.js
+++ build_schemas.js
@@ -4,10 +4,12 @@
  * downstream consumption to avoid FS calls in the browser.
  */
 const fs = require("fs");
+const path = require("path");
+const mergeAllOf = require("json-schema-merge-allof");
 const { ESSE } = require("./lib/js/esse");
 
 const esse = new ESSE();
-const { schemas } = esse;
+const { schemas, wrappedExamples } = esse;
 const schema = esse.buildGlobalSchema();
 
 fs.writeFileSync(
@@ -17,3 +19,30 @@ fs.writeFileSync(
 );
 
 fs.writeFileSync("./schema.js", "module.exports = " + JSON.stringify(schema), "utf8");
+
+if (process.env.BUILD_DOCS !== "true") {
+    process.exit(0);
+}
+schemas.forEach((schema) => {
+    if (!process.env.SKIP_MERGE_ALLOF === "true") {
+        schema = mergeAllOf(schema, {resolvers: {defaultResolver: mergeAllOf.options.resolvers.title}});
+    }
+    id_as_path = schema["$id"].replace("-", "_");
+    full_path = `./docs/js/schema${id_as_path}.json`;
+    fs.mkdirSync(path.dirname(full_path), {recursive: true})
+    fs.writeFileSync(
+        full_path,
+        JSON.stringify(schema, null, 4),
+        "utf8",
+    );
+})
+wrappedExamples.forEach((example) => {
+    id_as_path = example["path"].replace("-", "_");
+    full_path = `./docs/js/example${id_as_path}.json`;
+    fs.mkdirSync(path.dirname(full_path), {recursive: true})
+    fs.writeFileSync(
+        full_path,
+        JSON.stringify(example, null, 4),
+        "utf8",
+    );
+})
