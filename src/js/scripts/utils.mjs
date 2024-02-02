102a96f4d73207d25533bb419c0fc1bedfd986e0
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-02-02 16:56:44 +0200

    chore: add walkDirSync util

--- src/js/scripts/utils.mjs
+++ src/js/scripts/utils.mjs
@@ -1,6 +1,7 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
-import fs from "fs/promises";
+import fsPromises from "fs/promises";
+import fs from "fs";
 import path from "path";
 
 /**
@@ -9,16 +10,36 @@ import path from "path";
  * @param callback {() => void | Promise<void>}
  */
 export async function walkDir(dir, callback) {
-    const subDirs = await fs.readdir(dir);
+    const subDirs = await fsPromises.readdir(dir);
 
     for (const subDir of subDirs) {
         const itemPath = path.join(dir, subDir);
-        const stat = await fs.stat(itemPath);
+        const stat = await fsPromises.stat(itemPath);
 
         if (stat.isDirectory()) {
             await walkDir(itemPath, callback);
         } else {
             await callback(itemPath);
+        }
+    }
+}
+
+/**
+ *
+ * @param dir {String}
+ * @param callback {() => void}
+ */
+export function walkDirSync(dir, callback) {
+    const subDirs = fs.readdirSync(dir);
+
+    for (const subDir of subDirs) {
+        const itemPath = path.join(dir, subDir);
+        const stat = fs.statSync(itemPath);
+
+        if (stat.isDirectory()) {
+            walkDirSync(itemPath, callback);
+        } else {
+            callback(itemPath);
         }
     }
 }
